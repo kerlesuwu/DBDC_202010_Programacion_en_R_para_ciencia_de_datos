80360362debb30c0b570df261c3fe7a600a5f8ea
Miguel Jorquera Viguera <32852412+majorquev@users.noreply.github.com>
2020-11-18 21:12:16 -0300

    Add files via upload

---
+++ Clases/Clase5/Ejemplo_app/global.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(shinydashboard)#install.packages("tidyverse")
+library(tidyverse)
+library(highcharter)
+library(tidyr)
+library(nycflights13)
+
+data(flights)
+data(airports)
+data(airlines)
+data(planes)
+data(weather)
+
+vuelos      <- flights
+aeropuertos <- airports
+aerolineas  <- airlines
+aviones     <- planes
+clima       <- weather
+
+
+
+
+
